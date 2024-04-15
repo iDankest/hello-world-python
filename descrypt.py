ab77689c6c911eaf9577ab40ddd6ab4cb9f495c0
iDankest <DankestMirror@hotmail.es>
2024-04-16 00:25:56 +0100

    python

---
+++ descrypt.py
@@ -0,0 +1,28 @@
+print('Hello, Contosoville')
+
+#Esto es un comentario en python
+
+town = 'Vecindario'
+
+#imprimire la ciudad!
+
+print('The town is ' + town )
+
+def chant( phrase ):
+    print(phrase + phrase + phrase)
+
+chant('Vecindario!')
+
+def lasso_letter( letter, shift_amount ):
+    lasso_letter('a', 2)
+ord('a')
+
+def lasso_letter( letter, shift_amount ):
+    letter_code = ord(letter.lower())
+
+def lasso_letter( letter, shift_amount ):
+    letter_code = ord(letter.lower())
+    decoded_letter_code = letter_code + shift_amount
+    decoded_letter = chr(decoded_letter_code)
+    return decoded_letter
+print(lasso_letter('H', 2))
