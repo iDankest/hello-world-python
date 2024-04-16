da5648d1f6a40a5bba7d8a73c709ae30d3c1f34d
iDankest <DankestMirror@hotmail.es>
2024-04-16 19:55:48 +0100

    Date

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print('Hello, world!')
+
+a = 32
+b = 44
+if a < b:
+    print("Es mayor")
+fact = "The Moon has no atmosphere."
+fact + "No sound can be heard on the Moon."
+print(fact)
+multiline = "Facts about the Moon:\n There is no atmosphere.\n There is no sound."
+print(multiline)
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print(planets)
+print("There are", len(planets), "planets")
