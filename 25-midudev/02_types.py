1f8c47a8dfb1eccdc64de5c3be781d30d6fafe18
iDankest <kilijes@gmail.com>
2025-01-19 18:16:33 +0000

    02 y 03 done

---
+++ 25-midudev/02_types.py
@@ -0,0 +1,11 @@
+###
+# Loa tipos de datos en Python
+#int - Entero, float - Decimal, str - String, bool - Booleano, 
+# noneType - None, list - Lista, tuple - Tupla, 
+# dict - Diccionario, set - Conjunto, range - Rango
+
+print(type(5)) # int
+print(type(5.5)) # float
+print(type("Hola")) # str
+print(type(True)) # bool
+print(type(None)) # NoneType
---
+++ 25-midudev/03_cast.py
@@ -0,0 +1,18 @@
+###
+# 03 - casting de types
+# transformar de un tipo de dato a otro
+###
+
+# Ejecuta el script con python3 en lugar de python
+# En la terminal, usa el siguiente comando:
+# python3 /Users/idankest/code/hello-world-python/25-midudev/03_cast.py
+
+# Asegúrate de que el intérprete de Python 3 esté seleccionado en VS Code
+# Abre la paleta de comandos con Ctrl+Shift+P (Cmd+Shift+P en macOS)
+# Escribe "Python: Select Interpreter" y selecciona Python 3
+
+print("covercion de tipos de datos")
+
+print(2 + int("2")) # convierte el string a entero
+print("100" + str(2))
+print(bool(3))
